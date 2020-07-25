2f63376c31486b24ffba20c809e4d04d2444fad5
Nano Reperger <nanoreperger@gmail.com>
2020-07-25 14:44:57 -0300

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=daaeff417db3e6eab4b07c893f6a3587&query='+ latitude + ',' + longitude;
+
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback(body.error.info, undefined);
+        } else {
+            const data = body.current;
+            
+            const temp      = data.temperature;
+            const tempFeel  = data.feelslike;
+            const overcast = data.weather_descriptions[0];
+
+            callback(undefined, overcast + ". It is currently " + temp + " degrees out. It feels like " + tempFeel + " degrees out.");
+        }
+    });
+};
+
+module.exports =  forecast;
