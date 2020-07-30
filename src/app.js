bf05088aca61992709b994cb80fa4de54eb6fd76
Nano Reperger <nanoreperger@gmail.com>
2020-07-30 19:47:14 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
@@ -22,7 +22,7 @@ hbs.registerPartials(partialsPath);
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.use(express.bodyParser());
+//app.use(express.bodyParser());
 
 app.get('', (req, res) => {
     res.render('index', {
